eac85c4e836d0582f6a8eb35e7d7b9560369bf45
tommyngx <tommyfederation@gmail.com>
2024-12-27 18:29:50 +1100

    33

--- utils/gradcam.py
+++ utils/gradcam.py
@@ -39,7 +39,42 @@ def generate_gradcam_ori(model, image, target_layer):
     heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
     return heatmap
 
-def generate_gradcam(model, image, target_layer, image_weight=0.5):
+def generate_gradcam(model, image, target_layer):
+    model.eval()
+    if image.dim() == 3:
+        image = image.unsqueeze(0)
+    image.requires_grad = True
+
+    features = []
+    def hook_fn(module, input, output):
+        features.append(output)
+
+    handle = target_layer.register_forward_hook(hook_fn)
+    output = model(image)
+    handle.remove()
+
+    score = output[:, output.max(1)[-1]]
+    score.backward()
+
+    gradients = image.grad.data
+    pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
+    activations = features[0].detach()
+
+    for i in range(min(activations.shape[1], pooled_gradients.shape[0])):
+        activations[:, i, :, :] *= pooled_gradients[i]
+
+    heatmap = torch.mean(activations, dim=1).squeeze()
+    heatmap = F.relu(heatmap)
+    heatmap /= torch.max(heatmap)
+
+    heatmap = heatmap.cpu().numpy()  # Move to CPU before converting to NumPy
+    heatmap = cv2.resize(heatmap, (image.shape[2], image.shape[3]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
+    return heatmap
+
+def generate_gradcam22(model, image, target_layer, image_weight=0.5):
     """
     Generate Grad-CAM heatmap and blend it with the original image.
 
@@ -101,7 +136,7 @@ def generate_gradcam(model, image, target_layer, image_weight=0.5):
     heatmap = np.uint8(255 * heatmap)
 
     # Apply colormap to heatmap
-    #heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
     heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
 
     # Convert image tensor to NumPy array
