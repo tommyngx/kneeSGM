45ef34ddbb6391be848f377709d0a835aa8b3d2e
tommyngx <tommyfederation@gmail.com>
2024-12-27 20:18:32 +1100

    ee

--- utils/gradcam.py
+++ utils/gradcam.py
@@ -44,7 +44,7 @@ def generate_gradcam(model, image, target_layer):
     print(' step 1' ,heatmap.shape)
     heatmap_colored = np.stack([heatmap] * 3, axis=-1)
     print(' step 2' ,np.unique(heatmap_colored))
-    print(' step 4' ,heatmap.shape)
+    print(' step 4' ,heatmap_colored.shape)
     blue_mask = (heatmap_colored[:, :, 0] > 128) & (heatmap_colored[:, :, 1] < 50) & (heatmap_colored[:, :, 2] < 50)
     gray_value = heatmap[blue_mask]  # Use the normalized heatmap values as intensity for gray
     heatmap_colored[blue_mask] = np.stack([gray_value, gray_value, gray_value], axis=-1)
