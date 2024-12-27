9addb8f032403170193f0656400a8459b067381f
tommyngx <tommyfederation@gmail.com>
2024-12-27 19:25:01 +1100

    ee

--- utils/gradcam.py
+++ utils/gradcam.py
@@ -38,9 +38,10 @@ def generate_gradcam(model, image, target_layer):
 
     heatmap = heatmap.cpu().numpy()  # Move to CPU before converting to NumPy
     heatmap = cv2.resize(heatmap, (image.shape[2], image.shape[3]))
-    #heatmap = np.uint8(255 * heatmap)
+    heatmap = np.uint8(255 * heatmap)
+    print("afterww",heatmap.shape)
     heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-
+    print("after",heatmap.shape)
     return heatmap
 
 
