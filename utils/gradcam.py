d46347db4935c43db357282c3a1da23b0e65f701
tommyngx <tommyfederation@gmail.com>
2024-12-27 19:28:46 +1100

    ww

--- utils/gradcam.py
+++ utils/gradcam.py
@@ -40,7 +40,7 @@ def generate_gradcam(model, image, target_layer):
     heatmap = cv2.resize(heatmap, (image.shape[2], image.shape[3]))
     heatmap = np.uint8(255 * heatmap)
     print("afterww",heatmap.shape)
-    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    #heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
     print("after",heatmap.shape)
     return heatmap
 
