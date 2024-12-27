87670a76353ad65656f144ed6bcb377421aa905c
tommyngx <tommyfederation@gmail.com>
2024-12-27 17:44:01 +1100

    fix that

--- run/test_gradcam.py
+++ run/test_gradcam.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import yaml
 import os
 import argparse
+import matplotlib.pyplot as plt
 from models.model_architectures import get_model
 from data.data_loader import get_dataloader
 from utils.gradcam import save_random_predictions
@@ -21,7 +22,7 @@ def main(config_path='config/default.yaml', model_name=None, model_path=None, us
         model_name = config['model']['name']
     
     model = get_model(model_name, config_path=config_path, pretrained=config['model']['pretrained'])
-    checkpoint = torch.load(model_path)
+    checkpoint = torch.load(model_path, map_location=device)
     model.load_state_dict(checkpoint['model_state_dict'])
     model = model.to(device)
     
@@ -37,6 +38,9 @@ def main(config_path='config/default.yaml', model_name=None, model_path=None, us
     os.makedirs(output_dir, exist_ok=True)
     
     save_random_predictions(model, test_loader, device, output_dir, epoch=0, class_names=config['data']['class_names'], use_gradcam_plus_plus=use_gradcam_plus_plus)
+
+    # Display the plot
+    plt.show()
 
     # Keep only the last 3 latest saved epochs
     saved_files = sorted([f for f in os.listdir(output_dir) if f.startswith("random_predictions_epoch_")], reverse=True)
