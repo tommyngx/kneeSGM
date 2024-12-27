914faa7b1bd35e87a5ba512e1beadc0e2993cd69
tommyngx <tommyfederation@gmail.com>
2024-12-27 22:59:10 +1100

    ok

--- models/model_architectures.py
+++ models/model_architectures.py
@@ -15,16 +15,19 @@ def get_model(model_name, config_path='config/default.yaml', pretrained=True):
     
     if 'convnext_base' in model_name:
         model.head.fc = nn.Linear(model.head.fc.in_features, num_classes)
-    elif 'resnet50' in model_name or 'resnet101' in model_name: 
+    elif 'resnet50' in model_name or 'resnet101' in model_name: # test ok
         model.fc = nn.Linear(model.fc.in_features, num_classes)
     elif 'resnext50_32x4d' in model_name:
         model.fc = nn.Linear(model.fc.in_features, num_classes)
-    elif 'vit' in model_name:
+    elif 'vit_base_patch16_224' in model_name:
         model.head = nn.Linear(model.head.in_features, num_classes)
     elif 'densenet' in model_name:
         model.classifier = nn.Linear(model.classifier.in_features, num_classes)
-    elif 'caformer' in model_name:
+    elif 'caformer_s18' in model_name:
         model.head.fc = nn.Linear(model.head.fc.in_features, num_classes)
+    elif 'fastvit_t8' in model_name:
+        model.head.fc = nn.Linear(model.head.fc.in_features, num_classes)
+        
     else:
         raise ValueError(f"Model {model_name} not supported.")
     
