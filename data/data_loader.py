274a245f005195e3649aa1d9dc0592b02674c732
tommyngx <tommyfederation@gmail.com>
2024-12-29 18:55:59 +1100

    update oai16

--- data/data_loader.py
+++ data/data_loader.py
@@ -8,9 +8,18 @@ import yaml
 import os
 
 class KneeOsteoarthritisDataset(Dataset):
-    def __init__(self, csv_file, split, image_path_column, label_column, dataset_based_link, transform=None, config_path='config/default.yaml'):
+    def __init__(self, csv_file, split, image_path_column, label_column, dataset_based_link, dataset_name, external_datasets, transform=None, config_path='config/default.yaml'):
         self.data = pd.read_csv(csv_file)
+        self.data = self.data[self.data['data'] == dataset_name]
         self.data = self.data[self.data['split'] == split]
+        
+        if external_datasets:
+            for external_dataset in external_datasets.split(','):
+                external_data = pd.read_csv(csv_file)
+                external_data = external_data[external_data['data'] == external_dataset.strip()]
+                external_data['split'] = 'TRAIN'
+                self.data = pd.concat([self.data, external_data])
+        
         self.image_path_column = image_path_column
         self.label_column = label_column
         self.dataset_based_link = dataset_based_link
@@ -35,10 +44,13 @@ def load_config(config_path):
 def get_dataloader(split, batch_size, num_workers, transform=None, config_path='config/default.yaml'):
     config = load_config(config_path)
     csv_file = config['data']['metadata_csv']
+    dataset_name = config['data']['dataset_name']
+    external_datasets = config['data'].get('external_datasets', None)
     split = config['data'][split.lower() + '_split']
     image_path_column = config['data']['image_path_column']
     label_column = config['data']['label_column']
     dataset_based_link = config['data']['dataset_based_link']
-    dataset = KneeOsteoarthritisDataset(csv_file, split, image_path_column, label_column, dataset_based_link, transform=transform, config_path=config_path)
+    
+    dataset = KneeOsteoarthritisDataset(csv_file, split, image_path_column, label_column, dataset_based_link, dataset_name, external_datasets, transform=transform, config_path=config_path)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     return dataloader
