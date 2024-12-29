6dc3076f20f861c6021de889d46bb35066da01b7
tommyngx <tommyfederation@gmail.com>
2024-12-29 19:04:40 +1100

    update it

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,18 +1,28 @@
 from torchvision import transforms
+import yaml
 
-def get_transforms(image_size):
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+def get_transforms(image_size, config_path='config/default.yaml'):
+    config = load_config(config_path)
+    mean = config['transforms']['normalize']['mean']
+    std = config['transforms']['normalize']['std']
+    
     train_transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(10),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=mean, std=std)
     ])
 
     val_transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=mean, std=std)
     ])
 
     return train_transform, val_transform
