712e08f94903fe028a4a054afb3769e72157a0a0
tommyngx <tommyfederation@gmail.com>
2024-12-29 17:26:25 +1100

    start copy

--- data/process_oai16pre.py
+++ data/process_oai16pre.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import argparse
+from tqdm import tqdm
+import shutil
 
 def process_files(folder_path):
     train_path = os.path.join(folder_path, 'train.csv')
@@ -25,9 +27,25 @@ def process_files(folder_path):
     combined_df['filename'] = combined_df['filename'].str.replace(r'\.jpg$', '', regex=True)
     combined_df['path'] = combined_df['split'] + "/" + combined_df['filename'] + ".jpg"
     combined_df['path2'] = "OAI16/" + combined_df['split'] + "/" + combined_df['KL'].astype(int).astype(str) + "/" + combined_df['filename'] + ".png"
+    
     output_path = os.path.join(folder_path, 'OAI16metadata.csv')
     combined_df.to_csv(output_path, index=False)
     print(f"Combined file saved to {output_path}")
+    
+    # Load images from path and save to path2
+    for _, row in tqdm(combined_df.iterrows(), total=combined_df.shape[0]):
+        src_path = os.path.join(folder_path, row['path'])
+        dest_path = os.path.join(folder_path, row['path2'])
+        dest_dir = os.path.dirname(dest_path)
+        
+        if os.path.exists(dest_dir):
+            shutil.rmtree(dest_dir)
+        os.makedirs(dest_dir, exist_ok=True)
+        
+        if os.path.exists(src_path):
+            shutil.copy(src_path, dest_path)
+        else:
+            print(f"Source file {src_path} not found.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process train.csv and test.csv files.')
