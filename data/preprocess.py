cb4a24cf8c69b50fd88e3bad61c33e782ce13f1e
tommyngx <tommyfederation@gmail.com>
2024-12-30 16:21:03 +1100

    temporay reset augment

--- data/preprocess.py
+++ data/preprocess.py
@@ -13,4 +13,23 @@ def get_transforms(image_size, config_path='config/default.yaml'):
     train_transform = get_augmentations(config_path, split='train')
     val_transform = get_augmentations(config_path, split='val')
 
+    mean = config['transforms']['normalize']['mean']
+    std = config['transforms']['normalize']['std']
+
+    train_transform = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std)
+    ])
+
+    val_transform = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std)
+    ])
+
     return train_transform, val_transform
+
+
