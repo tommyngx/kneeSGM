370ab363b4554f819d3cd32b5a8d8fd5be0dd131
tommyngx <tommyfederation@gmail.com>
2024-12-30 16:30:50 +1100

    false augment

--- data/preprocess.py
+++ data/preprocess.py
@@ -10,25 +10,27 @@ def load_config(config_path):
 def get_transforms(image_size, config_path='config/default.yaml'):
     config = load_config(config_path)
     
-    train_transform = get_augmentations(config_path, split='train')
-    val_transform = get_augmentations(config_path, split='val')
-
+    use_augmentations = config['data'].get('use_augmentations', True)
     mean = config['data']['augmentations']['normalize']['mean']
     std = config['data']['augmentations']['normalize']['std']
 
-    #train_transform = transforms.Compose([
-    #    transforms.Resize((image_size, image_size)),
-    #    transforms.RandomHorizontalFlip(),
-    #    transforms.RandomRotation(10),
-    #    transforms.ToTensor(),
-    #    transforms.Normalize(mean=mean, std=std)
-    #])
+    if use_augmentations:
+        train_transform = get_augmentations(config_path, split='train')
+        val_transform = get_augmentations(config_path, split='val')
+    else:
+        train_transform = transforms.Compose([
+            transforms.Resize((image_size, image_size)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std)
+        ])
 
-    #val_transform = transforms.Compose([
-    #    transforms.Resize((image_size, image_size)),
-    #    transforms.ToTensor(),
-    #    transforms.Normalize(mean=mean, std=std)
-    #])
+        val_transform = transforms.Compose([
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std)
+        ])
 
     return train_transform, val_transform
 
