7377e31e3df4e7d929088aa7ff0a2f3dbef6aacd
tommyngx <tommyfederation@gmail.com>
2024-12-30 18:11:42 +1100

    test

--- detect/train_yolo.py
+++ detect/train_yolo.py
@@ -2,7 +2,6 @@ import os
 import subprocess
 import yaml
 import argparse
-from ultralytics import YOLO 
 
 def load_config(config_path):
     with open(config_path, 'r') as file:
@@ -13,7 +12,7 @@ def train_yolo(dataset_location):
     config = load_config('config/default.yaml')  # Update this path to your config file
     output_folder = config['output_dir'] + "/yolo"
     print("output_folder:", output_folder)
-    model = YOLO('yolo11s.pt')
+    
     command = [
         "yolo", "task=detect", "mode=train", "model=yolo11s.pt",
         f"data={dataset_location}/data.yaml", "epochs=10", "imgsz=640", "plots=True",
