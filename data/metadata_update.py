a996a1c6998734bb67f8d297e4941f6ace90233c
tommyngx <tommyfederation@gmail.com>
2024-12-31 18:16:14 +1100

    update metadata

--- data/metadata_update.py
+++ data/metadata_update.py
@@ -2,7 +2,7 @@ import os
 import argparse
 import pandas as pd
 
-def concatenate_metadata(folder_base, output_name):
+def concatenate_metadata(folder_base, output_name, save_link=None):
     all_metadata = []
 
     for subfolder in os.listdir(folder_base):
@@ -16,19 +16,26 @@ def concatenate_metadata(folder_base, output_name):
     
     if all_metadata:
         concatenated_metadata = pd.concat(all_metadata, ignore_index=True)
+        concatenated_metadata['split'] = concatenated_metadata['split'].replace('VAL', 'TEST')
         output_path = os.path.join(folder_base, output_name)
         concatenated_metadata.to_csv(f"{output_path}.csv", index=False)
         print(f"Concatenated metadata saved to {output_path}")
+        
+        if save_link:
+            with open(save_link, 'w') as f:
+                f.write(f"{output_path}.csv")
+            print(f"Link to metadata saved to {save_link}")
     else:
         print("No metadata files found.")
 
-def main(folder_base, output_name):
-    concatenate_metadata(folder_base, output_name)
+def main(folder_base, output_name, save_link=None):
+    concatenate_metadata(folder_base, output_name, save_link)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Concatenate metadata from multiple subfolders")
     parser.add_argument('--folder_base', type=str, required=True, help='Path to the base folder containing subfolders with metadata.csv')
     parser.add_argument('--output_name', type=str, required=True, help='Name of the output concatenated metadata file')
+    parser.add_argument('--save_link', type=str, help='Path to save the link to the concatenated metadata file')
     args = parser.parse_args()
     
-    main(args.folder_base, args.output_name)
+    main(args.folder_base, args.output_name, args.save_link)
