a38828bd580eaa2da0e72deaadd809cd1085d16d
tommyngx <tommyfederation@gmail.com>
2025-01-05 09:59:26 +1100

    fix argu

--- data/deidentify.py
+++ data/deidentify.py
@@ -3,6 +3,11 @@ import random
 import pydicom
 import argparse
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Get a random DICOM file and print its information.")
+    parser.add_argument('--folder', type=str, required=True, help='Path to the folder containing DICOM files.')
+    return parser.parse_args()
+
 def get_random_dcm_file(folder_path):
     print(f"Searching for DICOM files in folder: {folder_path}")
     dcm_files = [os.path.join(root, file) for root, _, files in os.walk(folder_path) for file in files if file.endswith('.dcm')]
@@ -16,11 +21,6 @@ def print_dcm_info(dcm_file):
     print(f"Information for DICOM file: {dcm_file}")
     for elem in ds:
         print(f"{elem.tag} {elem.name}: {elem.value}")
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Get a random DICOM file and print its information.")
-    parser.add_argument('--folder', type=str, required=True, help='Path to the folder containing DICOM files.')
-    return parser.parse_args()
 
 def main():
     args = parse_arguments()
